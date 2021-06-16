6a6d4c01066bf707dfd6d6c820520701a755ba21
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-16 17:51:37 -0500

    #contacto se importo el router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { routing, appRoutingProviders } from './app.routing'
+import { Router, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './components/material/material.module';
 import { LayoutModule } from './components/layout/layout.module';
 import { SectionsModule } from './components/sections/sections.module';
+import { ContactoComponent } from './components/layout/contacto/contacto.component';
+
+const router: Routes =[
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +29,8 @@ import { SectionsModule } from './components/sections/sections.module';
     MaterialModule,
 
     LayoutModule,
-    SectionsModule
+    SectionsModule,
+    RouterModule.forRoot(router)
   ],
   providers: [
     appRoutingProviders
